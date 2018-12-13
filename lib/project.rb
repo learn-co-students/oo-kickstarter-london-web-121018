83a5d724a62e8df19f72e00977d22167f7d52865
github username <github email address>
2018-12-13 13:19:51 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,25 @@
+
+class Backer
+ attr_accessor :name, :backed_projects
+ def initialize(name)
+  @name = name
+  @backed_projects = []
+ end
+ def back_project(project)
+  @backed_projects << project
+  project.backers << self
+ end
+end
+
+  
+class Project
+ attr_accessor :title, :backers
+ def initialize(title)
+  @title = title
+  @backers = []
+ end
+ def add_backer(backer)
+  @backers << backer
+  backer.backed_projects << self
+ end
+end
