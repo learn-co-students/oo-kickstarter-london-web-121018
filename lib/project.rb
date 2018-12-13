fa82e50bfaff788e88b03b95e9cdb5bec1d36f0d
github username <github email address>
2018-12-13 13:40:15 +0000

    first commit

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project
+
+attr_accessor :backers, :title
+
+def initialize(title, backers = [])
+  @title = title
+  @backers = backers
+end
+
+def add_backer(backer)
+  @backer = Backer.new(backer)
+  @backers << backer
+  backer.backed_projects << self
+end
+
+end
